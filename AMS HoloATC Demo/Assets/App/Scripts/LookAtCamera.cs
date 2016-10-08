9dc24dcf2a8430499302d748deae310c820563b6
Joost van Schaik <joost.van.schaik@wortell.nl>
2016-10-08 11:11:07 +0200

    Added blog 3 code and assets

---
+++ AMS HoloATC Demo/Assets/App/Scripts/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(Camera.main.transform);
+        gameObject.transform.Rotate(Vector3.up, 180f);
+    }
+}
